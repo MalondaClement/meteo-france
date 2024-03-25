e0959b56fb98f1b2757077793515ee8696d7dbbc
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2024-03-25 18:44:53 +0100

    add function for text and map requests with vigilance api

---
+++ meteo_france/vigilance/vigilance.py
@@ -0,0 +1,45 @@
+#
+#  vigilance/vigilance.py
+#
+#  Created by Clément Malonda on 25/03/2024.
+
+import requests
+
+from .config import BASIC_URL
+
+def get_vigilance(api_token: str) -> str:
+    if not api_token:
+        raise ValueError("Missing API token")
+    
+    request_url = BASIC_URL + "/textesvigilance/encours"
+    headers = {"accept": "*/*", "apikey": api_token}
+
+    response = requests.get(request_url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+def get_vigilance_map(api_token: str) -> str:
+    if not api_token:
+        raise ValueError("Missing API token")
+    
+    request_url = BASIC_URL + "/cartevigilance/encours"
+    headers = {"accept": "*/*", "apikey": api_token}
+
+    response = requests.get(request_url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+def get_vigilance_image_J(api_token: str):
+    pass
+
+def get_vigilance_image_J1(api_token: str):
+    pass
+
+def get_vigilance_image_J_J1(api_token: str):
+    pass
