797015c05664c3884cd0a07eed012a7c8ced76e3
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2024-03-26 15:22:50 +0100

    move api_token as a parameter in stations.py

--- meteo_france/climatology/stations.py
+++ meteo_france/climatology/stations.py
@@ -3,16 +3,11 @@
 #
 #  Created by Clément Malonda on 18/01/2024.
 
-import os
 import requests
-from dotenv import load_dotenv
 
 from .config import BASIC_URL
 
-def get_6m_stations_list(id_departement: int) -> str:
-    load_dotenv()
-    api_token = os.getenv("API_TOKEN")
-
+def get_6m_stations_list(id_departement: int, api_token: str) -> str:
     if not api_token:
         raise ValueError("API_TOKEN not found in environment variables.")
     
@@ -27,10 +22,7 @@ def get_6m_stations_list(id_departement: int) -> str:
     else:
         response.raise_for_status()
 
-def get_hourly_stations_list(id_departement: int) -> str:
-    load_dotenv()
-    api_token = os.getenv("API_TOKEN")
-
+def get_hourly_stations_list(id_departement: int, api_token: str) -> str:
     if not api_token:
         raise ValueError("API_TOKEN not found in environment variables.")
     
@@ -45,10 +37,7 @@ def get_hourly_stations_list(id_departement: int) -> str:
     else:
         response.raise_for_status()
 
-def get_daily_stations_list(id_departement: int) -> str:
-    load_dotenv()
-    api_token = os.getenv("API_TOKEN")
-
+def get_daily_stations_list(id_departement: int, api_token: str) -> str:
     if not api_token:
         raise ValueError("API_TOKEN not found in environment variables.")
     
@@ -63,10 +52,7 @@ def get_daily_stations_list(id_departement: int) -> str:
     else:
         response.raise_for_status()
 
-def get_station_info(id_station: str) -> str:
-    load_dotenv()
-    api_token = os.getenv("API_TOKEN")
-
+def get_station_info(id_station: str, api_token: str) -> str:
     if not api_token:
         raise ValueError("API_TOKEN not found in environment variables.")
     
--- tests/test_climatology/test_stations.py
+++ tests/test_climatology/test_stations.py
@@ -5,42 +5,63 @@
 
 import unittest
 
+import os
 import requests
+from dotenv import load_dotenv
 
 from meteo_france.climatology.stations import get_6m_stations_list, get_hourly_stations_list, get_daily_stations_list
 
 class TestStations(unittest.TestCase):
 
     def test_get_6m_stations_list_result(self):
-        result = get_6m_stations_list(94)
+        load_dotenv()
+        api_token = os.getenv("API_TOKEN")
+
+        result = get_6m_stations_list(94, api_token)
         theorical_result = {"id": "94042001", "nom": "JOINVILLE", "posteOuvert": True, "typePoste": 2, "lon": 2.462667, "lat": 48.813667, "alt": 37, "postePublic": True}
+        
         self.assertEqual(result[0], theorical_result)
 
     def test_get_6m_stations_list_param_error(self):
+        load_dotenv()
+        api_token = os.getenv("API_TOKEN")
+
         with self.assertRaises(requests.exceptions.HTTPError) as context:
-            result = get_6m_stations_list(0)
+            result = get_6m_stations_list(0, api_token)
             print(str(context.exception))
         self.assertEqual(str(context.exception), "404 Client Error: Not Found for url: https://public-api.meteofrance.fr/public/DPClim/v1/liste-stations/infrahoraire-6m?id-departement=0")
     
     def test_get_hourly_stations_list_result(self):
-        result = get_hourly_stations_list(94)
+        load_dotenv()
+        api_token = os.getenv("API_TOKEN")
+
+        result = get_hourly_stations_list(94, api_token)
         theorical_result = {"id": "94015002", "nom": "BRY-SPC", "posteOuvert": True, "typePoste": 5, "lon": 2.522667, "lat": 48.8335, "alt": 90, "postePublic": True}
         self.assertEqual(result[0], theorical_result)
 
     def test_get_hourly_list_param_error(self):
+        load_dotenv()
+        api_token = os.getenv("API_TOKEN")
+
         with self.assertRaises(requests.exceptions.HTTPError) as context:
-            result = get_hourly_stations_list(0)
+            result = get_hourly_stations_list(0, api_token)
             print(str(context.exception))
         self.assertEqual(str(context.exception), "404 Client Error: Not Found for url: https://public-api.meteofrance.fr/public/DPClim/v1/liste-stations/horaire?id-departement=0")
 
     def test_get_daily_stations_list_result(self):
-        result = get_daily_stations_list(94)
+        load_dotenv()
+        api_token = os.getenv("API_TOKEN")
+
+        result = get_daily_stations_list(94, api_token)
         theorical_result = {"id": "94004001", "nom": "BOISSY-ST-LEGER", "posteOuvert": False, "typePoste": 4, "lon": 2.515, "lat": 48.75, "alt": 95, "postePublic": True}
         self.assertEqual(result[0], theorical_result)
 
     def test_get_daily_stations_list_param_error(self):
+        load_dotenv()
+        api_token = os.getenv("API_TOKEN")
+
         with self.assertRaises(requests.exceptions.HTTPError) as context:
-            result = get_daily_stations_list(0)
+            result = get_daily_stations_list(0, api_token)
             print(str(context.exception))
         self.assertEqual(str(context.exception), "404 Client Error: Not Found for url: https://public-api.meteofrance.fr/public/DPClim/v1/liste-stations/quotidienne?id-departement=0")
 
